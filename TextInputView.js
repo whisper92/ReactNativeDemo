20f9c0d06fc97d7f404eb6131db20d444484ca5b
wangxianpeng <wisimer@163.com>
2015-08-23 21:52:05 +0800

    textinput

--- TextInputView.js
+++ TextInputView.js
@@ -1,35 +1,90 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
 'use strict';
 
 var React = require('react-native');
+
+
 var {
   StyleSheet,
-  Image,
-  View,
   Text,
-  Component
+  View,
+  TextInput
 } = React;
+
 
 var styles = StyleSheet.create({
   container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#F5FCFF',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
-  description: {
-    fontSize: 18,
-    margin: 5,
-    color: '#656565'
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+
+var Input = React.createClass({
+
+    handleUpdateChange(text) {
+        this.props.updateChange(text);
+    },
+
+
+  render() {
+
+    return (
+        <TextInput onChangeText={(text) => this.handleUpdateChange(text)} style={{ width : 200, height: 40, borderColor: 'gray', borderWidth: 1}} />
+    );
   }
 });
-class TextView extends Component {
+
+
+
+
+var ShowText = React.createClass({
+
+    getInitialState(){
+      return {
+        text : '我是文字'
+      }
+    },
+
+    handleChange(textValue){
+      this.setState({
+        text: textValue
+      });
+    },
+
+    render() {
+
+        return (
+          <View style={{flex : 1,justifyContent: 'center',alignItems: 'center',backgroundColor: '#F5FCFF'}}>
+            <Text>{this.state.text}</Text>
+            <Input updateChange={this.handleChange} />
+          </View>
+        );
+    }
+});
+
+module.exports = React.createClass({
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.description}>TextInput</Text>
+        <ShowText />
       </View>
     );
   }
-};
-
-module.exports = TextView;
+});
